a0e64f7c93d87b56e5c60928915528ac16e59a5b
CHAK <Marco@Marcos-MacBook.local>
2021-01-28 16:13:43 +0200

    added jquery, added mobile menu, edited css

---
+++ JS/scripts.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+    var mySwiper = new Swiper('.swiper-container', {
+        // Optional parameters
+        loop: true,
+      
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+          },
+      });
+
+// SECOND SWIPER
+
+      var mySwiper = new Swiper('.review-container', {
+        // Optional parameters
+        loop: true,
+
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+
+        },
+
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+          },
+      });
+
+$(".header").on("click", ".menu-button", function() {
+    $(".header").toggleClass('menu-opened');
+})
+
+})
