3374e4d3c44e01d3c1d93314499113652f33563d
CHAK <Marco@Marcos-MacBook.local>
2021-01-28 16:15:54 +0200

    edited css html and js doc's, code has become readeble

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,46 +1,46 @@
-$(document).ready(function(){
-    var mySwiper = new Swiper('.swiper-container', {
-        // Optional parameters
-        loop: true,
-      
-        // If we need pagination
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
-        },
-      
-        // Navigation arrows
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-        autoplay: {
-            delay: 3000,
-            disableOnInteraction: false,
-          },
-      });
+$(document).ready(function () {
+  var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    loop: true,
 
-// SECOND SWIPER
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
 
-      var mySwiper = new Swiper('.review-container', {
-        // Optional parameters
-        loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+  });
 
-        // If we need pagination
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
+  // SECOND SWIPER
 
-        },
+  var mySwiper = new Swiper('.review-container', {
+    // Optional parameters
+    loop: true,
 
-        autoplay: {
-            delay: 3000,
-            disableOnInteraction: false,
-          },
-      });
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
 
-$(".header").on("click", ".menu-button", function() {
+    },
+
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+  });
+
+  $(".header").on("click", ".menu-button", function () {
     $(".header").toggleClass('menu-opened');
-})
+  })
 
 })
